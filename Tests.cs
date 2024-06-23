eb88970f24379c72ea78d1b07d38b725b33a6a88
Copashka <Vasayka345@gmail.com>
2024-06-23 23:25:26 +0300

    выав

--- Tests.cs
+++ Tests.cs
@@ -29,8 +29,6 @@ namespace MiSPIC.Xunit
 
     public class ForTestsClass
     {
-        public const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\FrogmoneyCasinoDatabase.mdf;Integrated Security=True";
-        
         public static string HashPassword(string password)
         {
             using (SHA256 sha256 = SHA256.Create())
@@ -45,14 +43,14 @@ namespace MiSPIC.Xunit
                 return builder.ToString();
             }
         }
-        public LoginOption ReturnUserRegistrationStatus(string username, string password, SqlConnection connection)
+        public LoginOption ReturnUserRegistrationStatus(string username, string password, SqlConnection connection, SqlCommand command)
         {
             using (connection)
             {
                 connection.Open();
 
                 string isUserRegistered = "SELECT COUNT( * ) FROM UserList WHERE Name = @Name AND Password = @Password;";
-                using (SqlCommand command = new SqlCommand(isUserRegistered, connection))
+                using (command)
                 {
                     command.Parameters.AddWithValue("@Name", username);
                     command.Parameters.AddWithValue("@Password", HashPassword(password));
@@ -65,7 +63,7 @@ namespace MiSPIC.Xunit
                 }
 
                 string isPasswordWrong = "SELECT COUNT( * ) FROM UserList WHERE Name = @Name;";
-                using (SqlCommand command = new SqlCommand(isPasswordWrong, connection))
+                using (command)
                 {
                     command.Parameters.AddWithValue("@Name", username);
                     int count = (int)command.ExecuteScalar();
@@ -78,11 +76,29 @@ namespace MiSPIC.Xunit
                 return LoginOption.Register;
             }
         }
+        public UserStatus GetUserAccessLevel(string username, SqlConnection connection, SqlCommand command)
+        {
+            using (connection)
+            {
+                connection.Open();
 
+                string getUserStatusQuery = "SELECT AccessLevel FROM UserList WHERE Name = @Name;";
+                using (command)
+                {
+                    command.Parameters.AddWithValue("@Name", username);
+
+                    object accessStatus = command.ExecuteScalar();
+                    if (accessStatus != null && Enum.TryParse<UserStatus>(accessStatus.ToString(), out UserStatus status))
+                    {
+                        return status;
+                    }
+                }
+            }
+            return UserStatus.Ludik;
+        }
     }
 
-
-    public class ReturnUserRegistrationStatus
+    public class ReturnUserRegistrationStatusTest
     {
         [Fact]
         public void ReturnUserRegistrationStatus_ExistingUserCorrectPassword_ReturnsLoginOptionLogin()
@@ -100,7 +116,7 @@ namespace MiSPIC.Xunit
             var forTestsClass = new ForTestsClass();
 
             // Act
-            var result = forTestsClass.ReturnUserRegistrationStatus(usernameText, passwordText, mockConnection.Object);
+            var result = forTestsClass.ReturnUserRegistrationStatus(usernameText, passwordText, mockConnection.Object, mockCommand.Object);
 
             // Assert
             Assert.Equal(LoginOption.Login, result);
@@ -122,10 +138,10 @@ namespace MiSPIC.Xunit
             var forTestsClass = new ForTestsClass();
 
             // Act
-            var result = forTestsClass.ReturnUserRegistrationStatus(usernameText, passwordText, mockConnection.Object);
+            var result = forTestsClass.ReturnUserRegistrationStatus(usernameText, passwordText, mockConnection.Object, mockCommand.Object);
 
             // Assert
-            Assert.Equal(LoginOption.Login, result);
+            Assert.Equal(LoginOption.WrongPasswordLogin, result);
         }
 
         [Fact]
@@ -144,10 +160,118 @@ namespace MiSPIC.Xunit
             var forTestsClass = new ForTestsClass();
 
             // Act
-            var result = forTestsClass.ReturnUserRegistrationStatus(usernameText, passwordText, mockConnection.Object);
+            var result = forTestsClass.ReturnUserRegistrationStatus(usernameText, passwordText, mockConnection.Object, mockCommand.Object);
 
             // Assert
-            Assert.Equal(LoginOption.Login, result);
+            Assert.Equal(LoginOption.Register, result);
         }
     }
+ 
+    public class GetUserAccessLevelTest
+    {
+        [Fact]
+        public void ReturnUserAccessLevel_ExistingUserHasAdminRole_ReturnsAdminUserStatus()
+        {
+            // Arrange
+            string usernameText = "AdminUser";
+
+            var mockConnection = new Mock<SqlConnection>();
+            var mockCommand = new Mock<SqlCommand>();
+
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+            mockCommand.Setup(cmd => cmd.ExecuteScalar()).Returns(1);
+
+            var forTestsClass = new ForTestsClass();
+
+            // Act
+            var result = forTestsClass.GetUserAccessLevel(usernameText, mockConnection.Object, mockCommand.Object);
+
+            // Assert
+            Assert.Equal(UserStatus.Admin, result);
+        }
+
+        [Fact]
+        public void ReturnUserAccessLevel_ExistingUserHasSportRole_ReturnsSportUserStatus()
+        {
+            // Arrange
+            string usernameText = "SportUser";
+
+            var mockConnection = new Mock<SqlConnection>();
+            var mockCommand = new Mock<SqlCommand>();
+
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+            mockCommand.Setup(cmd => cmd.ExecuteScalar()).Returns(1);
+
+            var forTestsClass = new ForTestsClass();
+
+            // Act
+            var result = forTestsClass.GetUserAccessLevel(usernameText, mockConnection.Object, mockCommand.Object);
+
+            // Assert
+            Assert.Equal(UserStatus.Sport, result);
+        }
+
+        [Fact]
+        public void ReturnUserAccessLevel_ExistingUserHasVIPRole_ReturnsVIPUserStatus()
+        {
+            // Arrange
+            string usernameText = "VIPUser";
+
+            var mockConnection = new Mock<SqlConnection>();
+            var mockCommand = new Mock<SqlCommand>();
+
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+            mockCommand.Setup(cmd => cmd.ExecuteScalar()).Returns(1);
+
+            var forTestsClass = new ForTestsClass();
+
+            // Act
+            var result = forTestsClass.GetUserAccessLevel(usernameText, mockConnection.Object, mockCommand.Object);
+
+            // Assert
+            Assert.Equal(UserStatus.VIP, result);
+        }
+
+        [Fact]
+        public void ReturnUserAccessLevel_ExistingUserHasPokerClubMemberRole_ReturnsPokerClubMemberUserStatus()
+        {
+            // Arrange
+            string usernameText = "PokerClubMember";
+
+            var mockConnection = new Mock<SqlConnection>();
+            var mockCommand = new Mock<SqlCommand>();
+
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+            mockCommand.Setup(cmd => cmd.ExecuteScalar()).Returns(1);
+
+            var forTestsClass = new ForTestsClass();
+
+            // Act
+            var result = forTestsClass.GetUserAccessLevel(usernameText, mockConnection.Object, mockCommand.Object);
+
+            // Assert
+            Assert.Equal(UserStatus.PokerClubMember, result);
+        }
+
+        [Fact]
+        public void ReturnUserAccessLevel_ExistingUserHasLudikRole_ReturnsLudikUserStatus()
+        {
+            // Arrange
+            string usernameText = "LudikUser";
+
+            var mockConnection = new Mock<SqlConnection>();
+            var mockCommand = new Mock<SqlCommand>();
+
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+            mockCommand.Setup(cmd => cmd.ExecuteScalar()).Returns(1);
+
+            var forTestsClass = new ForTestsClass();
+
+            // Act
+            var result = forTestsClass.GetUserAccessLevel(usernameText, mockConnection.Object, mockCommand.Object);
+
+            // Assert
+            Assert.Equal(UserStatus.Ludik, result);
+        }
+    } 
 }
